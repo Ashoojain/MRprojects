538945161ff9debeebe68e33c068630c6f802511
Jain Ashoo (RBEI/BSW2) <jis2kor@bosch.com>
2019-02-21 16:03:45 +0530

    Initial commit

---
+++ Assets/scripts/ballmovement.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class ballmovement : MonoBehaviour
+{
+    public Rigidbody rb;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, 700*Time.deltaTime);
+
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(600 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-600 * Time.deltaTime, 0, 0);
+        }
+    }
+}
